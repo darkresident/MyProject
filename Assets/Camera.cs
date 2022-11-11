36b89f6058a71caf22552cf5982fd54465adeead
darkresident <sitnikovden@gmail.com>
2022-11-11 04:25:33 +0300

    Add player

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -23,5 +23,3 @@ public class Camera : MonoBehaviour
         else return;
     }
 }
-
-}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,3 +1,18 @@
-﻿public class Player
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
